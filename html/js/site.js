a8d4d2528ae427bdabc3fe2fabbb106807c4562c
findkong <findkong@gmail.com>
2014-05-15 22:23:07 +1000

    Create Repository

---
+++ html/js/site.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var tagline = document.querySelector("p.tagline");
+    tagline.innerText = "From Cloud9 IDE Test I need a more complex piece of JS!";
+});
